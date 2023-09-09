230e794177abbc36e2b23988b40535a83b589ea2
quanglopxe <quanglopxe@gmail.com>
2023-09-09 10:20:44 +0700

    MVC Tuan 3

---
+++ Bai1_B/Bai1_B/Models/Information.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bai1_B.Models
+{
+    public class Information
+    {
+        public string FullName { get; set; }
+        public string IdStudent { get; set; }
+        public string Email { get; set; }
+        public string FileImage { get; set; }
+        public string Note { get; set; }
+        public bool Check1 { get; set; }
+        public bool Check2 { get; set; }
+        public bool Check3 { get; set; }
+        public string ChooseWorkTime { get; set; }
+        public int SelectCourse { get; set; }
+    }
+}
